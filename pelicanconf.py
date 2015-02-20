5f610069391db416e100dd6e1e8ea2e51cf2160c
Rhys Tyers <rhystyers1@gmail.com>
2015-02-20 14:31:02 +0000

    Tidied conf file. Added favicon

--- pelicanconf.py
+++ pelicanconf.py
@@ -24,16 +24,6 @@ FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 
-# Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
-
-# Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
-
 DEFAULT_PAGINATION = False
 
 DEFAULT_DATE_FORMAT = '%d-%m-%Y'
@@ -41,8 +31,9 @@ DEFAULT_DATE_FORMAT = '%d-%m-%Y'
 PLUGIN_PATHS = ["plugins", "plugins"]
 PLUGINS = ['photoarchive']
 
-# Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+SLUGIFY_SOURCE = 'basename'
 
 ARTICLE_URL = 'articles/{slug}.html'
 ARTICLE_SAVE_AS = 'articles/{slug}.html'
+
+FAVICON = "assets/iclogo.png"
