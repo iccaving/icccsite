2fb7463cd2df66a4deac999ecb9e0c67f80b6be3
Rhys Tyers <rhystyers1@gmail.com>
2015-03-18 22:23:19 +0000

    Added nzrocks article by Tanguy

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-AUTHOR = u'Stores Gnomes'
+AUTHOR = u'ICCC'
 SITENAME = u'Imperial College Caving Club'
 #SITEURL = 'https://union.ic.ac.uk/rcc/caving/topsecret'
 SITEURL = ''
