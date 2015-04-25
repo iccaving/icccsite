acdb4fea6c8bf3a3233a93a437ddbae435fba9be
Rhys Tyers <rhystyers1@gmail.com>
2015-04-25 15:31:28 +0100

    update rsync scripts to remove wx permission from others as apparently this is not allowed for php files (also it is unneccesary

--- jh_rsync.sh
+++ jh_rsync.sh
@@ -1,1 +1,1 @@
-rsync -avz --delete --chmod=a+rwx output/ jhdavies@dougal.union.ic.ac.uk:/home/www/htdocs/rcc/caving/newzealand
+rsync -avz --delete --chmod=ug+rwx,o-wx,o+r output/ jhdavies@dougal.union.ic.ac.uk:/home/www/htdocs/rcc/caving/newzealand
