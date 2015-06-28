39efea5b3aa6c1634b313dbc4a742912dc6d776a
Rhys Tyers <rhystyers1@gmail.com>
2015-06-28 21:38:47 +0100

    Various theme tweaks

--- rsync.sh
+++ rsync.sh
@@ -1,1 +1,1 @@
-rsync -avz --delete --chmod=ug+rwx,o-wx,o+r output/ rt811@dougal.union.ic.ac.uk:/home/www/htdocs/rcc/caving/newzealand
+rsync -avz --chmod=ug+rwx,o-wx,o+r output/ rt811@dougal.union.ic.ac.uk:/home/www/htdocs/rcc/caving/topsecret
