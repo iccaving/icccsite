d3e45c0a5f328c7346f2d35e169bcc7c0b4dff3a
Rhys Tyers <rhystyers1@gmail.com>
2015-09-04 20:09:59 +0100

    Minor change to get link path construction right

--- plugins/photoarchive/photoarchive.py
+++ plugins/photoarchive/photoarchive.py
@@ -18,14 +18,9 @@ def isarchive(generator):
                     archive_loc += article.date.strftime('%Y-%m-%d') + '%20-%20' + article.location + '/'
                 article.metadata['archiveloc'] = archive_loc.lower()
                 article.archiveloc = archive_loc.lower()
-                logging.critical(archive_loc)
             else:
-                if article.photoarchive[:1] == '/':
                     article.metadata['archiveloc'] = archive_loc + article.photoarchive + '/'
                     article.archiveloc = archive_loc + article.photoarchive + '/'
-                else:
-                    article.metadata['archiveloc'] = article.photoarchive + '/'
-                    article.archiveloc = article.photoarchive + '/'
             archives.append((article.archiveloc, article.title, article.date.strftime('%d-%m-%Y')))
     generator.context['photoarchives'] = archives
 
