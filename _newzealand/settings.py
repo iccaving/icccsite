0f2154664a6866018d7d75b9c8fe6bd227e8e9e8
Rhys Tyers <rhystyers1@gmail.com>
2015-09-04 20:07:41 +0100

    Modified subsites so that (blinker) signal mappings are removed before and after a subsite is generated. This prevented plugins from being run on sites they shouldn't be run on. This also prevents the subsites from recursively running the subsites plugin and so the ISSUBSITE setting was redundant and therefore removed

--- _newzealand/settings.py
+++ _newzealand/settings.py
@@ -2,12 +2,15 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-ISSUBSITE = True
 PATH = 'content'
 SUBSITE_PATH = 'newzealand'
 
 THEME = 'themes/NZTheme'
 
 STATIC_PATHS = ['assets', 'photo_archive']
+
+PLUGIN_PATHS = ['plugins']
+
+PLUGINS = ['photoarchive', 'saveoldnz', 'inlinephotos']
 
 ARTICLE_EXCLUDES = ['oldnz']
