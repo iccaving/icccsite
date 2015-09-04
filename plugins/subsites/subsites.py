6a11f50245f0a4a40fdec100c87d74f1b6786ebd
Rhys Tyers <rhystyers1@gmail.com>
2015-09-04 20:07:41 +0100

    Modified subsites so that (blinker) signal mappings are removed before and after a subsite is generated. This prevented plugins from being run on sites they shouldn't be run on. This also prevents the subsites from recursively running the subsites plugin and so the ISSUBSITE setting was redundant and therefore removed

--- plugins/subsites/subsites.py
+++ plugins/subsites/subsites.py
@@ -50,36 +50,41 @@ def subsites(pelican_obj):
     basesettings = pelican_obj.settings.copy()
     baseoutput = basesettings["OUTPUT_PATH"]
     basesite = basesettings["SITEURL"]
-    # Check if a subsite is running this. If so, stop. I don't know why this is
-    # necessary.
-    if "ISSUBSITE" in basesettings.keys() and basesettings["ISSUBSITE"] == True:
-        logging.debug("Subsite: Recursion protection activated")
-    else:
-        # Run through the list of subsites
-        for subsite in basesettings["SUBSITES"]:
-            # Turn the settings file into a dictionary and make the relative paths
-            # absolute
-            newsettings = relativise_path(
-                get_settings_from_file(subsite + "/settings.py", basesettings), subsite + "/settings.py")
-            # Ensure that the output is a subdirectory of the main site
-            newsettings["OUTPUT_PATH"] = baseoutput + \
-                "/" + newsettings["SUBSITE_PATH"]
-            # Ensure that the subsite knows its url
-            newsettings["SITEURL"] = basesite + \
-                "/" + newsettings["SUBSITE_PATH"]
-            # Pelican magic settings checker
-            settings = configure_settings(newsettings)
-            # Set up a pelican class
-            cls = basesettings['PELICAN_CLASS']
-            # I think this is some python2/3 compatibility thing
-            # I'm scared to remove.
-            if isinstance(cls, six.string_types):
-                module, cls_name = cls.rsplit('.', 1)
-                module = __import__(module)
-                cls = getattr(module, cls_name)
-            # Create a new pelican instance for the subsite and run!
-            new_pelican_obj = cls(settings)
-            new_pelican_obj.run()
+    # Run through the list of subsites
+    for subsite in basesettings["SUBSITES"]:
+        # Turn the settings file into a dictionary and make the relative paths
+        # absolute
+        newsettings = relativise_path(
+            get_settings_from_file(subsite + "/settings.py", basesettings), subsite + "/settings.py")
+        # Ensure that the output is a subdirectory of the main site
+        newsettings["OUTPUT_PATH"] = baseoutput + \
+            "/" + newsettings["SUBSITE_PATH"]
+        # Ensure that the subsite knows its url
+        newsettings["SITEURL"] = basesite + \
+            "/" + newsettings["SUBSITE_PATH"]
+        # Pelican magic settings checker
+        settings = configure_settings(newsettings)
+        # This is a bit hacky. It removes the plugin to signal mapping. For
+        # reason even in a new pelican object the signals are still mapped
+        # to the old plugins.
+        # iterate through module's attributes
+        for name, val in signals.__dict__.iteritems():
+            if hasattr(val, 'receivers'):
+                val.receivers = {}
+        # Set up a pelican class
+        cls = basesettings['PELICAN_CLASS']
+        # I think this is some python2/3 compatibility thing
+        # I'm scared to remove.
+        if isinstance(cls, six.string_types):
+            module, cls_name = cls.rsplit('.', 1)
+            module = __import__(module)
+            cls = getattr(module, cls_name)
+        # Create a new pelican instance for the subsite and run!
+        new_pelican_obj = cls(settings)
+        new_pelican_obj.run()
+        for name, val in signals.__dict__.iteritems():
+            if hasattr(val, 'receivers'):
+                val.receivers = {}
 
 
 def register():
