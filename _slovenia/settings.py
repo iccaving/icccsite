f22d1901a3d1f616a86184f56cc1b278a2f2b9e2
Rhys Tyers <rhystyers1@gmail.com>
2015-09-17 01:14:55 +0100

    Theme fixes

---
+++ _slovenia/settings.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+PATH = 'content'
+SUBSITE_PATH = 'slovenia'
+
+THEME = 'themes/SlovTheme'
+
+STATIC_PATHS = ['assets', 'photo_archive']
+
+PLUGIN_PATHS = ['plugins']
+
+PLUGINS = ['photoarchive', 'inlinephotos']
