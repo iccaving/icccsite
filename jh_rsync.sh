bf5ab9c31033b0928afb787c58c0901b7d38cbd1
jdhare <jack.d.hare@gmail.com>
2015-11-13 21:23:59 +0000

    updates for W2

--- jh_rsync.sh
+++ jh_rsync.sh
@@ -1,1 +1,1 @@
-rsync -avz --delete --chmod=ug+rwx,o-wx,o+r output/ jhdavies@dougal.union.ic.ac.uk:/home/www/htdocs/rcc/caving/newzealand
+rsync -avz -e "ssh -p 10022" --chmod=ug+rwx,o-wx,o+r output/ jhdavies@dougal.union.ic.ac.uk:/home/www/htdocs/rcc/caving
