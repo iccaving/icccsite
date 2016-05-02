f336e4c7df60d814457b0f1e993ec083df950286
Rhys Tyers <rhystyers1@gmail.com>
2016-05-02 12:50:15 +0100

    Some refactoring to try to improve speed

--- plugins/acyear/acyear.py
+++ plugins/acyear/acyear.py
@@ -5,35 +5,32 @@ import logging
 # This plugin is intended to enable populating the sidebar with trips sorted by
 # academic year
 
+def acyearinit(generator):
+    generator.context['yearlist'] = []
 
-def acyear(generator):
+def acyear(generator, content):
     # Create a list of article objects and their associated academic year
-    yearlist = []
+    article = content
     row = namedtuple('row', 'date acyear article')
-    for article in generator.articles:
-        if 'type' in article.metadata.keys():
-            # Only do this if the article is a trip because it only matters for the sidebar
-            # ordering
-            if article.type == 'trip' and 'unlisted' not in article.metadata.keys():
-                # The boundary is august/september
-                if int(article.date.strftime('%m')) > 8:
-                    year1 = article.date.strftime('%Y')
-                    year2 = str(int(article.date.strftime('%Y')) + 1)
-                    yearlist.append(
-                        row(article.date, year1 + '-' + year2, article))
-                else:
-                    year1 = str(int(article.date.strftime('%Y')) - 1)
-                    year2 = article.date.strftime('%Y')
-                    yearlist.append(
-                        row(article.date, year1 + '-' + year2, article))
-
-    # Sort the list by date (the full date, not academic year) and make
-    # available to generator context (so the templates can access it)
-    generator.context['yearlist'] = sorted(
-        yearlist, key=lambda x: x.date, reverse=True)
-    logging.debug("acyear: Success!")
+    if 'type' in article.metadata.keys():
+        # Only do this if the article is a trip because it only matters for the sidebar
+        # ordering
+        if article.type == 'trip' and 'unlisted' not in article.metadata.keys():
+            # The boundary is august/september
+            if int(article.date.strftime('%m')) > 8:
+                year1 = article.date.strftime('%Y')
+                year2 = str(int(article.date.strftime('%Y')) + 1)
+                generator.context['yearlist'].append(
+                    row(article.date, year1 + '-' + year2, article))
+            else:
+                year1 = str(int(article.date.strftime('%Y')) - 1)
+                year2 = article.date.strftime('%Y')
+                generator.context['yearlist'].append(
+                    row(article.date, year1 + '-' + year2, article))
 
 
 def register():
-    # After the list of articles has been generated, do acyear
-    signals.article_generator_finalized.connect(acyear)
+    #Set up the acyear list
+    signals.article_generator_init.connect(acyearinit)
+    # After each article is processed add it to the acyear list
+    signals.article_generator_write_article.connect(acyear)
