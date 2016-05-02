87f48633fbb76965cf96a0897370b0373073f17e
Rhys Tyers <rhystyers1@gmail.com>
2016-05-02 17:18:56 +0100

    Made pagination nicer

--- pelicanconf.py
+++ pelicanconf.py
@@ -20,6 +20,9 @@ DEFAULT_LANG = u'en'
 CATEGORY_SAVE_AS = ''
 TAG_SAVE_AS = ''
 
+AUTHOR_URL            = 'author/{slug}/'
+AUTHOR_SAVE_AS        = 'author/{slug}/index.html'
+
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
@@ -27,6 +30,10 @@ TRANSLATION_FEED_ATOM = None
 
 DEFAULT_PAGINATION = 10
 DEFAULT_ORPHANS = 4
+PAGINATION_PATTERNS = (
+    (1, '{base_name}/', '{base_name}/index.html'),
+    (2, '{base_name}/page/{number}/', '{base_name}/page/{number}/index.html'),
+)
 
 DEFAULT_DATE_FORMAT = '%d-%m-%Y'
 
