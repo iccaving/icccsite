d75159e0dd104beea0ece2861394db1c7bec7db5
rhyst <rhystyers1@gmail.com>
2016-10-12 22:02:41 +0100

    Adding in sftp client and batch file for deploying from windows

---
+++ win_deploy.bat
@@ -0,0 +1,9 @@
+@ECHO OFF
+set /p username=Username?:
+REM Get passoword in a nice-ish masked way
+powershell -Command $pword = read-host "Enter password" -AsSecureString ; $BSTR=[System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($pword) ; [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR) > .tmp.txt & set /p password=<.tmp.txt & del .tmp.txt
+echo open %username%@dougal.union.ic.ac.uk 10022 > psftp.txt
+REM echo put -r output /home/users/website/rcc/caving
+REM echo %CD% >> psftp.txt
+psftp.exe -pw %password% -b psftp.txt
+if exist psftp.txt del psftp.txt
