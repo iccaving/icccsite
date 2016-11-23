720e69f2652d4793c292f6b81af1e29a9b4594cb
Rhys Tyers <rhystyers1@gmail.com>
2016-11-23 07:46:50 +0000

    fixes

--- plugins/inlinephotos/mdx_inlinephotos.py
+++ plugins/inlinephotos/mdx_inlinephotos.py
@@ -35,9 +35,9 @@ class AttrTagPattern(Pattern):
                     archive_loc += str(self.md.Meta['type'][0]) + 's/'
                 if 'location' in self.md.Meta.keys() and 'date' in self.md.Meta.keys():
                     archive_loc += str(self.md.Meta['date'][0]) + '%20-%20' + str(self.md.Meta['location'][0]) + '/'
+                archive_loc = archive_loc.lower()
             elif 'photoarchive' in self.md.Meta.keys() and self.md.Meta['photoarchive'][0] != '':
                 archive_loc = self.md.Meta['photoarchive'][0]
-            archive_loc = archive_loc.lower()
             if len(m.group(7).split(",")) > 1:
                 img.set('src', archive_loc + m.group(7).split(",")[0].strip())
                 a.set('href', m.group(7).split(",")[1].strip())
