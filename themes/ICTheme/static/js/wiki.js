be52af4509b7b6e80c129579fc1995f38e11937e
Rhys Tyers <rhystyers1@gmail.com>
2016-12-21 22:16:01 +0000

    tree -> menu

--- themes/ICTheme/static/js/wiki.js
+++ themes/ICTheme/static/js/wiki.js
@@ -1,7 +1,7 @@
 window.addEventListener("load", function() {
   document.querySelector(".wiki-nav-button").addEventListener('click', function(event) {
     document.querySelector(".wiki-links").classList.toggle('nodisplay');
-    event.target.text = event.target.text == "Expand Tree" ? "Collapse Tree" : "Expand Tree";
+    event.target.text = event.target.text == "Expand Menu" ? "Collapse Menu" : "Expand Menu";
   }, false);
   var expanders = document.querySelectorAll(".wiki-dir-expander");
   for (i = 0; i < expanders.length; ++i) {
