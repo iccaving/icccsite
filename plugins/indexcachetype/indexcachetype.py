ef34ea3d4fb2296d758497ed0fae2bc8625296f1
Rhys Tyers <rhystyers1@gmail.com>
2018-04-21 22:57:57 +0100

    Add null check

--- plugins/indexcachetype/indexcachetype.py
+++ plugins/indexcachetype/indexcachetype.py
@@ -2,9 +2,10 @@ import os
 
 def add_cache_type(sender, context, articles):
     for afile in context['all_files']:
-        path = afile.relpath.split(os.sep)
-        if len(path) > 0 and 'index' == path[0]:
-            afile.cache_type = "INDEX_ARTICLE"
+        if afile.relpath:
+            path = afile.relpath.split(os.sep)
+            if len(path) > 0 and 'index' == path[0]:
+                afile.cache_type = "INDEX_ARTICLE"
 
 
 def register():
