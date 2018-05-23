448a280a1d1cbf979156909c2e573f73c6e92082
Rhys Tyers <rhystyers1@gmail.com>
2018-05-23 15:19:59 +0100

    debug

--- plugins/cavepeeps/bios.py
+++ plugins/cavepeeps/bios.py
@@ -224,12 +224,15 @@ def generate_cave_pages(context):
     row=namedtuple('row', 'name number recentdate meta')
     rows = []
     for cave in context['caves_db']:
-        print(cave)
-        name = cave['name']
-        number = len(cave['article'].cave_articles)
-        recentdate = max([trip[1] for trip in cave['article'].cave_articles])
-        meta = cave['article'].metadata
-        rows.append(row(name, number, recentdate, meta))
+        try:
+            name = cave['name']
+            number = len(cave['article'].cave_articles)
+            recentdate = max([trip[1] for trip in cave['article'].cave_articles])
+            meta = cave['article'].metadata
+            rows.append(row(name, number, recentdate, meta))
+        except Exception as e:
+            print(cave)
+            print(e)
     filename=os.path.join('caves', 'index.html')
     
     writer = Writer(
